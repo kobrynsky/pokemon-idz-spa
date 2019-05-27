5b39f005ba4fd457d5f6d466df4b8e6fdbb4f79b
blc132 <pawelkobrynski97@gmail.com>
2019-05-27 05:52:41 +0200

    Czyszczenie kodu, dodanie obrazka w tle offline

--- src/App.js
+++ src/App.js
@@ -1,48 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 import Menu from "./components/Menu/Menu";
-import HomePage from "./components/HomePage/HomePage";
-import AppliedRoute from "./components/Others/AppliedRoute";
-import NotFound from "./components/Others/NotFound";
-import { Route, Switch, BrowserRouter } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.css';
 import Routes from "./Routes";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isAuthenticated: false,
-      id: 0,
-      email: "",
-      login: "",
-    };
-  }
-
-  userHasAuthenticated = authenticated => {
-    this.setState({ isAuthenticated: authenticated });
-  }
-
   render() {
-    const childProps = {
-      isAuthenticated: this.state.isAuthenticated,
-      userHasAuthenticated: this.userHasAuthenticated,
-      userId: this.id,
-      userEmail: this.email,
-      userLogin: this.login
-    };
-
     return (
       <div className="App">
         <div className="content">
-          <Menu props={childProps} />
-          <Routes childProps={childProps} />
+          <Menu/>
+          <Routes/>
         </div>
       </div>
     );
   }
-
 }
-
 export default App;
