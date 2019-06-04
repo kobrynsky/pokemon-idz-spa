3ff5813f291487cd05a89437e6d444aba4a047f7
blc132 <pawelkobrynski97@gmail.com>
2019-06-04 15:53:04 +0200

    Stylizacja strony 404

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./NotFound.css";
+import PsyDuckImg from '../../assets/psyduck.png';
 export default () =>
     <div className="NotFound">
         <h3>Nie znaleziono strony!</h3>
+        <img src={PsyDuckImg} alt="psyduck"></img>
     </div>;
